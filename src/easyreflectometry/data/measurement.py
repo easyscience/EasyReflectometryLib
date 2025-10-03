55d9f75e35f0a122f87315dad43506e5d206731b
Piotr Rozyczko <piotr.rozyczko@gmail.com>
2025-10-03 21:07:29 +0200

    check/mask zero variances before the fitting. the datastore should contain pristine datasets

--- src/easyreflectometry/data/measurement.py
+++ src/easyreflectometry/data/measurement.py
@@ -1,7 +1,6 @@
 __author__ = 'github.com/arm61'
 
 import os
-import warnings
 from typing import TextIO
 from typing import Union
 
@@ -32,38 +31,11 @@ def load_as_dataset(fname: Union[TextIO, str]) -> DataSet1D:
     coords_name = 'Qz_' + basename
     coords_name = list(data_group['coords'].keys())[0] if coords_name not in data_group['coords'] else coords_name
     data_name = list(data_group['data'].keys())[0] if data_name not in data_group['data'] else data_name
-
-    # Get the data arrays
-    x_vals = data_group['coords'][coords_name].values
-    y_vals = data_group['data'][data_name].values
-    ye_vals = data_group['data'][data_name].variances
-    xe_vals = data_group['coords'][coords_name].variances
-
-    # Check for zero variances and remove those points
-    # Only check ye_vals for zero if xe_vals was all zeros (3-column file)
-    if np.all(xe_vals == 0.0):
-        zero_variance_mask = (ye_vals == 0.0)
-    else:
-        zero_variance_mask = (ye_vals == 0.0) | (xe_vals == 0.0)
-    num_zero_variance = np.sum(zero_variance_mask)
-
-    if num_zero_variance > 0:
-        warnings.warn(
-            f"Removed {num_zero_variance} data point(s) from '{fname}' due to zero variance.",
-            UserWarning
-        )
-        # Keep only points with non-zero variances
-        valid_mask = ~zero_variance_mask
-        x_vals = x_vals[valid_mask]
-        y_vals = y_vals[valid_mask]
-        ye_vals = ye_vals[valid_mask]
-        xe_vals = xe_vals[valid_mask]
-
     return DataSet1D(
-        x=x_vals,
-        y=y_vals,
-        ye=ye_vals,
-        xe=xe_vals,
+        x=data_group['coords'][coords_name].values,
+        y=data_group['data'][data_name].values,
+        ye=data_group['data'][data_name].variances,
+        xe=data_group['coords'][coords_name].variances,
     )
 
 
@@ -100,50 +72,6 @@ def _load_orso(fname: Union[TextIO, str]) -> sc.DataGroup:
                 variances=np.square(o.data[:, 2]),
             )
         attrs[f'R_{name}'] = {'orso_header': sc.scalar(Header.asdict(o.info))}
-
-    # Check for zero variances and remove those points
-    total_removed = 0
-    for key in list(data.keys()):
-        data_variances = data[key].variances
-        # Find the corresponding coordinate key for this data
-        coord_key = None
-        for coord_name in coords.keys():
-            if data[key].dims == coords[coord_name].dims:
-                coord_key = coord_name
-                break
-
-        if coord_key is None:
-            continue  # Skip if no matching coordinate found
-
-        coord_variances = coords[coord_key].variances
-
-        # Find points with zero variances
-        zero_variance_mask = (data_variances == 0.0) | (coord_variances == 0.0)
-        num_zero_variance = np.sum(zero_variance_mask)
-        total_removed += num_zero_variance
-
-        if num_zero_variance > 0:
-            # Keep only points with non-zero variances
-            valid_mask = ~zero_variance_mask
-            data[key] = sc.array(
-                dims=data[key].dims,
-                values=data[key].values[valid_mask],
-                variances=data[key].variances[valid_mask],
-                unit=data[key].unit if hasattr(data[key], 'unit') else None
-            )
-            coords[coord_key] = sc.array(
-                dims=coords[coord_key].dims,
-                values=coords[coord_key].values[valid_mask],
-                variances=coords[coord_key].variances[valid_mask],
-                unit=coords[coord_key].unit
-            )
-
-    if total_removed > 0:
-        warnings.warn(
-            f"Removed {total_removed} data point(s) from '{fname}' due to zero variance.",
-            UserWarning
-        )
-
     return sc.DataGroup(data=data, coords=coords, attrs=attrs)
 
 
@@ -192,37 +120,12 @@ def _load_txt(fname: Union[TextIO, str]) -> sc.DataGroup:
 
     data_name = 'R_' + basename
     coords_name = 'Qz_' + basename
-
-    # Calculate variances
-    y_variances = np.square(e)
-    x_variances = np.square(xe)
-
-    # Check for zero variances and remove those points
-    # Only check y_variances for zero if xe was all zeros (3-column file)
-    if np.all(xe == 0.0):
-        zero_variance_mask = (y_variances == 0.0)
-    else:
-        zero_variance_mask = (y_variances == 0.0) | (x_variances == 0.0)
-    num_zero_variance = np.sum(zero_variance_mask)
-
-    if num_zero_variance > 0:
-        warnings.warn(
-            f"Removed {num_zero_variance} data point(s) from '{fname}' due to zero variance.",
-            UserWarning
-        )
-        # Keep only points with non-zero variances
-        valid_mask = ~zero_variance_mask
-        x = x[valid_mask]
-        y = y[valid_mask]
-        y_variances = y_variances[valid_mask]
-        x_variances = x_variances[valid_mask]
-
-    data = {data_name: sc.array(dims=[coords_name], values=y, variances=y_variances)}
+    data = {data_name: sc.array(dims=[coords_name], values=y, variances=np.square(e))}
     coords = {
         data[data_name].dims[0]: sc.array(
             dims=[coords_name],
             values=x,
-            variances=x_variances,
+            variances=np.square(xe),
             unit=sc.Unit('1/angstrom'),
         )
     }
