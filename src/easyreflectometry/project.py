b2886223158b0f9d13e7e291345b2ac2177c1c97
rozyczko <piotr.rozyczko@gmail.com>
2026-02-11 08:35:14 +0100

    temporarily switch off pointwise

--- src/easyreflectometry/project.py
+++ src/easyreflectometry/project.py
@@ -19,11 +19,11 @@ from easyreflectometry.data import DataSet1D
 from easyreflectometry.data import load_as_dataset
 from easyreflectometry.fitting import MultiFitter
 
-# from easyreflectometry.model import LinearSpline
+from easyreflectometry.model import LinearSpline
 from easyreflectometry.model import Model
 from easyreflectometry.model import ModelCollection
 from easyreflectometry.model import PercentageFwhm
-from easyreflectometry.model import Pointwise
+# from easyreflectometry.model import Pointwise
 from easyreflectometry.sample import Layer
 from easyreflectometry.sample import Material
 from easyreflectometry.sample import MaterialCollection
@@ -72,11 +72,20 @@ class Project:
 
     @property
     def parameters(self) -> List[Parameter]:
-        """Get all parameters from all models in the project."""
+        """Get all unique parameters from all models in the project.
+
+        Parameters shared across multiple models (e.g. material SLD) are
+        only included once to avoid double-counting.
+        """
         parameters = []
+        seen_ids: set[int] = set()
         if self._models is not None:
             for model in self._models:
-                parameters.extend(model.get_parameters())
+                for param in model.get_parameters():
+                    pid = id(param)
+                    if pid not in seen_ids:
+                        seen_ids.add(pid)
+                        parameters.append(param)
         return parameters
 
     @property
@@ -349,15 +358,15 @@ class Project:
 
         # Set the resolution function if variance data is present
         if sum(new_experiment.ye) != 0:
-            q = new_experiment.x
-            reflectivity = new_experiment.y
-            q_error = new_experiment.xe
-            # TODO: set resolution function based on value of control in GUI
-            resolution_function = Pointwise(q_data_points=[q, reflectivity, q_error])
-            # resolution_function = LinearSpline(
-            #     q_data_points=self._experiments[new_index].y,
-            #     fwhm_values=np.sqrt(self._experiments[new_index].ye),
-            # )
+            # q = new_experiment.x
+            # reflectivity = new_experiment.y
+            # q_error = new_experiment.xe
+            # # TODO: set resolution function based on value of control in GUI
+            # resolution_function = Pointwise(q_data_points=[q, reflectivity, q_error])
+            resolution_function = LinearSpline(
+                q_data_points=self._experiments[new_index].y,
+                fwhm_values=np.sqrt(self._experiments[new_index].ye),
+            )
             self.models[model_index].resolution_function = resolution_function
 
     def load_experiment_for_model_at_index(self, path: Union[Path, str], index: Optional[int] = 0) -> None:
@@ -369,14 +378,14 @@ class Project:
 
         # Set the resolution function if variance data is present
         if sum(self._experiments[index].ye) != 0:
-            q = self._experiments[index].x
-            reflectivity = self._experiments[index].y
-            q_error = self._experiments[index].xe
-            resolution_function = Pointwise(q_data_points=[q, reflectivity, q_error])
-            # resolution_function = LinearSpline(
-            #     q_data_points=self._experiments[index].y,
-            #     fwhm_values=np.sqrt(self._experiments[index].ye),
-            # )
+            # q = self._experiments[index].x
+            # reflectivity = self._experiments[index].y
+            # q_error = self._experiments[index].xe
+            # resolution_function = Pointwise(q_data_points=[q, reflectivity, q_error])
+            resolution_function = LinearSpline(
+                q_data_points=self._experiments[index].y,
+                fwhm_values=np.sqrt(self._experiments[index].ye),
+            )
             self._models[index].resolution_function = resolution_function
 
     def sld_data_for_model_at_index(self, index: int = 0) -> DataSet1D:
