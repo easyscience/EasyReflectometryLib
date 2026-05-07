9b6805124f3cd42352e37a3bb11b40cde519999d
rozyczko <piotr.rozyczko@gmail.com>
2026-05-07 15:17:54 +0200

    a few more fixes

--- src/easyreflectometry/__init__.py
+++ src/easyreflectometry/__init__.py
@@ -1,5 +1,6 @@
 # SPDX-FileCopyrightText: 2026 EasyScience contributors <https://github.com/easyscience>
 # SPDX-License-Identifier: BSD-3-Clause
+
 """EasyReflectometry library."""
 
 from importlib import metadata
--- tests/test_orso_utils.py
+++ tests/test_orso_utils.py
@@ -1,5 +1,5 @@
+# SPDX-FileCopyrightText: 2025 EasyScience contributors <https://github.com/easyscience>
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright (c) 2025 DMSC
 
 import os
 import warnings
