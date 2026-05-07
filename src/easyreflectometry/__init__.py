07d792cf001ea79c713937fed09f4c4af816389a
rozyczko <piotr.rozyczko@gmail.com>
2026-05-07 14:39:58 +0200

    remade __init__.py

--- src/easyreflectometry/__init__.py
+++ src/easyreflectometry/__init__.py
@@ -1,3 +1,17 @@
 # SPDX-FileCopyrightText: 2026 EasyScience contributors <https://github.com/easyscience>
 # SPDX-License-Identifier: BSD-3-Clause
 """EasyReflectometry library."""
+
+from importlib import metadata
+
+from .project import Project
+
+try:
+    __version__ = metadata.version(__package__ or __name__)
+except metadata.PackageNotFoundError:
+    __version__ = '0.0.0'
+
+__all__ = [
+    Project,
+    __version__,
+]
